a8fd4d6aa3ea27ad910f5ad96f2dbcf05f4c7aef
wy8881 <yi.wang02@student.adelaide.edu.au>
2024-10-28 02:33:19 +1030

    Update app_websocket.py

--- app_websocket.py
+++ app_websocket.py
@@ -11,7 +11,7 @@ import torch
 
 # Initialize Flask app
 app = Flask(__name__)
-socketio = SocketIO(host='0.0.0.0',debug=True,cors_allowed_origins='*')
+socketio = SocketIO(app,debug=True,cors_allowed_origins='*')
 
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -182,4 +182,4 @@ def process_image(image, houseId):
 
 
 if __name__ == '__main__':
-   socketio.run(host='0.0.0.0', port=5000)
+   socketio.run(app, host='0.0.0.0', port=5000, debug=True)
